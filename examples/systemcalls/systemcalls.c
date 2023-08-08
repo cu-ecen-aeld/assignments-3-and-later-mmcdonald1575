b1ef7f7d9981e91e55321d838e9827362c0fdb49
admin <mmcdonald@Ubuntu.myguest.virtualbox.org>
2023-08-08 13:15:51 -0400

    Resolved conflicts while reverting 4e4945b

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,163 +1,153 @@
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "systemcalls.h"
-//#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
-
-
-//@param int count;
-//@param char* $cmd[];
-//@return bool;
-
-
+/**
+ * @param cmd the command to execute with system()
+ * @return true if the command in @param cmd was executed
+ *   successfully using the system() call, false if an error occurred,
+ *   either in invocation of the system() call, or if a non-zero return
+ *   value was returned by the command issued in @param cmd.
+*/
 bool do_system(const char *cmd)
 {
-	int result;
-	result = system(cmd);
 
-	if(WIFEXITED(result))
-	{
-		if(WEXITSTATUS(result)==0)
-		{
-    			return true;
-    		}
-    		else
-    		{
-    			return false;
-    		}
-	}
-	else
-	{
-		return false;
-	}
+/*
+ * TODO  add your code here
+ *  Call the system() function with the command set in the cmd
+ *   and return a boolean true if the system() call completed with success
+ *   or false() if it returned a failure
+*/
+
+    int result = system(cmd);
+    if (result == -1) {
+        return false;
+    }
+    return WIFEXITED(result) && WEXITSTATUS(result) == 0;
 }
 
+/**
+* @param count -The numbers of variables passed to the function. The variables are command to execute.
+*   followed by arguments to pass to the command
+*   Since exec() does not perform path expansion, the command to execute needs
+*   to be an absolute path.
+* @param ... - A list of 1 or more arguments after the @param count argument.
+*   The first is always the full path to the command to execute with execv()
+*   The remaining arguments are a list of arguments to pass to the command in execv()
+* @return true if the command @param ... with arguments @param arguments were executed successfully
+*   using the execv() call, false if an error occurred, either in invocation of the
+*   fork, waitpid, or execv() command, or if a non-zero return value was returned
+*   by the command issued in @param arguments with the specified arguments.
+*/
 
 bool do_exec(int count, ...)
 {
+    
+/*
+ * TODO:
+ *   Execute a system command by calling fork, execv(),
+ *   and wait instead of system (see LSP page 161).
+ *   Use the command[0] as the full path to the command to execute
+ *   (first argument to execv), and use the remaining arguments
+ *   as second argument to the execv() command.
+ *
+*/
+	
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
 
-    command[count] = command[count];
-    
-    va_end(args);
-     
-     int execResult = 0;
-     pid_t pid_for_exec;
-     int wait_result = 0;
-     
-     pid_for_exec = fork();
-     
-     
-     if (pid_for_exec == 0)
-     {
-     	execResult = execv(command[0], command);
-     	exit(-1);
-     	
-     }
-     else
-     {
-      	return false;
-     }
-     
-     wait_result = waitpid(pid_for_exec, &execResult, 0);  
-     if (wait_result < 0)
-     {
-     	return false;
-     }   
-     if(WIFEXITED(execResult) && (WEXITSTATUS(execResult)==0))
-     {
-    	return true;
-     }
-     
-     else
-     {
-	return false;
-     }
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        perror("Fork failed");
+        va_end(args);
+        return false;
+    }
+    else if (pid == 0) // Child process
+    {
+        execv(command[0], command);
+        // execv only returns if there's an error
+        perror("Execv failed");
+        _exit(EXIT_FAILURE); // Use _exit to avoid atexit handlers
+    }
+    else // Parent process
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        va_end(args);
+        return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+    }
 }
 
-
+/**
+* @param outputfile - The full path to the file to write with command output.
+*   This file will be closed at completion of the function call.
+* All other parameters, see do_exec above
+*/
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+
+
+/*
+ * TODO
+ *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+ *   redirect standard out to a file specified by outputfile.
+ *   The rest of the behaviour is same as do_exec()
+ *
+*/
+
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
 
-    command[count] = command[count];
-	
-    va_end(args);	
-	
-    pid_t kidpid;
-    int execFileResult;
-    int fd = 0;
-    int fd2 = 0;
-    int pidresult = 0;
-    
-    fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
-
-if (fd < 0) 
-{ 
-	perror("open"); 
-	return false; 
-}
-
-kidpid = fork();
-if (kidpid < 0) 
-{
-  	//perror("fork"); 
-  	return false;
-}
-else
-{
-    fd2 = dup2(fd, 1);
-    close(fd);
-    if (fd2 < 0) 
-    { 
-    	perror("dup2"); 
-    	exit(-1); 
-    }
-    else
+    pid_t pid = fork();
+    if (pid == -1)
     {
-    	//close(fd);
-    	execv(command[0], command);
-    	perror("execv");
-    	exit(-1);
-    	pidresult = waitpid(kidpid,&execFileResult,0); 
-    	//close(fd);
+        perror("Fork failed");
+        va_end(args);
+        return false;
     }
-    if(pidresult == -1)
+    else if (pid == 0) // Child process
     {
-    	return false;
+        FILE *outfile = fopen(outputfile, "w");
+        if (outfile == NULL)
+        {
+            perror("Failed to open output file");
+            _exit(EXIT_FAILURE);
+        }
+
+        // Redirect standard output to the output file
+        dup2(fileno(outfile), STDOUT_FILENO);
+
+        execv(command[0], command);
+        // execv only returns if there's an error
+        perror("Execv failed");
+        fclose(outfile); // Close the output file before exiting
+        _exit(EXIT_FAILURE); // Use _exit to avoid atexit handlers
     }
-    if(WIFEXITED(execFileResult))
-	{
-		 if(WEXITSTATUS(execFileResult)==0)
-		 {
-    			return true;
-    		 }
-    		 else
-    		 {
-    		 	return false;
-		 }
-	}
-    else
-	{
-		return false;
-	}
+    else // Parent process
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        va_end(args);
+        return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+    }
 }
-}
+
