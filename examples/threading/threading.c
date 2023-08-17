347d0a861a90a74c4fe1c069f351a06c209702f7
admin <mmcdonald@Ubuntu.myguest.virtualbox.org>
2023-08-17 16:32:59 -0400

    Revert "assi4.1"
    This reverts commit 25009b7c7c8829870ee8a909eb47b2c4a4dc063a.

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -3,53 +3,66 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
 {
-    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
 
-    usleep(thread_func_args->wait_to_obtain_ms * 1000); // Convert to microseconds for usleep
-
-    if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
-        ERROR_LOG("Failed to lock the mutex");
-        thread_func_args->thread_complete_success = false;
-        return thread_func_args;
-    }
-
-    usleep(thread_func_args->wait_to_release_ms * 1000); // Convert to microseconds for usleep
-
-    if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
-        ERROR_LOG("Failed to unlock the mutex");
-        thread_func_args->thread_complete_success = false;
-        return thread_func_args;
-    }
-
-    thread_func_args->thread_complete_success = true;
-    return thread_func_args;
+	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+	
+	int result = 0;
+	int second_result = 0;
+	thread_func_args->thread_complete_success =false;
+	
+	usleep((thread_func_args->wait_to_obtain_ms));
+	//mutex_lock_result = pthread_mutex_lock(thread_func_args->mutex);
+	result = pthread_mutex_lock(thread_func_args->mutex);
+	
+	if (result == 0)
+	{
+		usleep((thread_func_args->wait_to_release_ms));
+		second_result = pthread_mutex_unlock(thread_func_args->mutex);
+		if (second_result == 0)
+		{
+		  thread_func_args->thread_complete_success =true;
+		}
+	}
+    return thread_param;
 }
 
-bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms)
+
+bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    struct thread_data *thread_data_ptr = malloc(sizeof(struct thread_data));
-    if (!thread_data_ptr) {
-        ERROR_LOG("Failed to allocate memory for thread_data");
-        return false;
-    }
+	//pthread_t p1;
+	//pthead_mutex_t *mutex;
+	//int wait_to_obtain_ms;
+	//int wait_to_release_ms;
+	
+	struct thread_data* data = (struct thread_data*)malloc(sizeof(struct thread_data));
+	
+	data->mutex = mutex;
+	data->wait_to_obtain_ms = wait_to_obtain_ms;
+	data->wait_to_release_ms = wait_to_release_ms;
+	data->p1 = thread;
+	
+	int success = 0;
+	
+	success = pthread_create(thread, NULL, threadfunc, data);
+	
+	if (success != 0)
+	{
+		//free(data);
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 
-    thread_data_ptr->mutex = mutex;
-    thread_data_ptr->wait_to_obtain_ms = wait_to_obtain_ms;
-    thread_data_ptr->wait_to_release_ms = wait_to_release_ms;
-    thread_data_ptr->thread_complete_success = false; // Initialize to false
-
-    if (pthread_create(thread, NULL, threadfunc, thread_data_ptr) != 0) {
-        ERROR_LOG("Failed to create thread");
-        free(thread_data_ptr); // Don't forget to free allocated memory in case of error
-        return false;
-    }
-
-    return true;
 }
-
+// adding comment to update commit
+// new commit
+// again
